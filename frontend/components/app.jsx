e92f461982cffde73e873b9633e74075d86e9d20
MichaelNgCen <michaelngcen@yahoo.com>
2022-08-15 17:31:07 -0400

    push 8.0

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,8 +1,9 @@
 import React from "react";
-const App = () => {
-    return (
-        <h1>hesadasda</h1>
-    );
+
+class App extends React.Component {
+    render() {
+        return (<h1>hello </h1>);
+    };
 };
 
 export default App;
