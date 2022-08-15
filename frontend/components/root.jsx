5f85776d6b9bfac80e21016703dde3d62411d5de
MichaelNgCen <michaelngcen@yahoo.com>
2022-08-15 17:14:58 -0400

    push 8.0

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Provider } from 'react-redux';
 import { HashRouter } from "react-router-dom";
-import App from "./App";
+// import App from "./App";
 
 const Root = ({ store }) => (
   <Provider store={store}>
     <HashRouter>
-      <App />
+      {/* <App /> */}
+      <h1>hello 2</h1>
     </HashRouter>
   </Provider>
 );
